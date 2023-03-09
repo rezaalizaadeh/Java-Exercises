f89c3d6a030f73293ace3dc177db1e83ac99576a
rezaalizaadeh <112873129+rezaalizaadeh@users.noreply.github.com>
2023-03-09 14:17:25 +0100

    Add files via upload

---
+++ Simple App/src/app/main.java
@@ -0,0 +1,21 @@
+package app;
+
+import javax.swing.JOptionPane;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		String name = JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null,"Hello "+name);
+		
+		int age =Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+		JOptionPane.showMessageDialog(null,"You are " + age+" years old"+" !");
+		
+		double height =Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+		JOptionPane.showMessageDialog(null,"You are " + height+" cm tall");
+				
+		
+	}
+
+}
